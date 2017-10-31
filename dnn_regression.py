2cc6b7741d3853334ffb85cdc18f30b18f3831da
Liorpe <liorp2905@gmail.com>
2017-10-31 20:12:50 +0200

    message

--- dnn_regression.py
+++ dnn_regression.py
@@ -22,7 +22,7 @@ import tensorflow as tf
 import pandas as pd
 import numpy as np
 import parse_description_file
-
+import os
 import itertools
 
 features_dict = parse_description_file.extract_feature_values('data/data_description.txt')
@@ -30,7 +30,6 @@ feature_cols = parse_description_file.create_features_lists(features_dict)
 FEATURES = list(features_dict.keys())
 
 LABEL = 'SalePrice'
-test_set = pd.read_csv('data/test.csv')
 tf.logging.set_verbosity(tf.logging.INFO)
 
 
@@ -58,37 +57,41 @@ def get_input_fn_predict(data_set, num_epochs=None, shuffle=True):
         shuffle=shuffle)
 
 
-def train_model(regressor):
+def train_model(regressor,preprocessed_data_directory):
     global training_set
     global test_set
     global features_dict
     global feature_cols
     global FEATURES
     global LABEL
-    training_set = pd.read_csv('data/train.csv')
+    training_set = pd.read_csv(os.path.join(preprocessed_data_directory,'train.csv'))
     training_set = convert_categoriacl_values_to_string(training_set,features_dict)
     regressor.train(input_fn=get_input_fn(training_set), steps=10000)
 
 
 
-def load_model(regressor):
-    global test_set
+def get_predictions_for_test_set(regressor, preprocessed_data_directory):
+    test_set =pd.read_csv(os.path.join(preprocessed_data_directory,'test.csv'))
     test_set = convert_categoriacl_values_to_string(test_set,features_dict)
     y = regressor.predict(input_fn=get_input_fn_predict(test_set,num_epochs=1,shuffle=False))
     id = 1461
-    with open ("predictions.txt", mode='w') as file:
+    with open (os.path.join(preprocessed_data_directory,"predictions.txt"), mode='w') as file:
         file.write("Id,SalePrice\n")
         for prediction in y:
             print(prediction['predictions'][0])
             file.write(f"{id},{prediction['predictions'][0]}\n")
             id+=1
 
-regressor = tf.estimator.DNNRegressor(feature_columns=feature_cols,
-                                          hidden_units=[1024, 512, 256],
-                                          model_dir='model_dir',
-                                          optimizer=tf.train.ProximalAdagradOptimizer(
-                                              learning_rate=0.2,
-                                              l1_regularization_strength=0.001
-                                          ))
-train_model(regressor)
-load_model(regressor)
+
+def train_and_get_predictions(preprocessed_data_directory):
+    regressor = tf.estimator.DNNRegressor(feature_columns=feature_cols,
+                                              hidden_units=[1024,512,256],
+                                              model_dir=os.path.join(preprocessed_data_directory,'model_dir'),
+                                              optimizer=tf.train.ProximalAdagradOptimizer(
+                                                  learning_rate=0.05,
+                                                  l1_regularization_strength=0.001
+                                              ))
+    train_model(regressor,preprocessed_data_directory)
+    get_predictions_for_test_set(regressor,preprocessed_data_directory)
+
+train_and_get_predictions(preprocessed_data_directory="preprocessed_data/example")
